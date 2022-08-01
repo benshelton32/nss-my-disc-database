5edd236cfea30fded06d60ee3dae5d7261fe3152
Ben Shelton <bks-44@hotmail.com>
2022-08-01 00:51:07 -0500

    added logo, favicon, and styling fot BagList, NavBar, and DiscForm

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,24 +6,27 @@ export const NavBar = () => {
     const navigate = useNavigate()
 
     return (
-        <ul className="navbar">
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/addDisc">Add Disc</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/myBag">My Bag</Link>
-            </li>
+        <section className="navbar">
+            <div className="navbar__item navbar-left">
+                <button className="addDiscButton" onClick={() => navigate("/addDisc")}>Add Disc</button>
+            </div>
+            <div className="navbar_brand"><img className="logo" src={process.env.PUBLIC_URL+"/myDiscDatabaseLogo.png"} /></div>
+            <div className="navbar-right">
+                <div className="navbar__item navbar__logout">
+                    <Link className="navbar__link" to="/myBag"><img className="navbar_icon" src={process.env.PUBLIC_URL+"/MDDbbag.png"}/><p className="navbar_icon_text">My Bag</p></Link>
+                </div>
 
-            {
-                localStorage.getItem("disc_user")
-                    ? <li className="navbar__item navbar__logout">
-                        <Link className="disc__link" to="" onClick={() => {
-                            localStorage.removeItem("disc_user")
-                            navigate("/", {replace: true})
-                        }}>Logout</Link>
-                    </li>
-                    : ""
-            }
-        </ul>
+                {
+                    localStorage.getItem("disc_user")
+                        ? <div className="navbar__item navbar__logout">
+                            <Link className="navbar__link" to="" onClick={() => {
+                                localStorage.removeItem("disc_user")
+                                navigate("/", {replace: true})
+                            }}><img className="navbar_icon" src={process.env.PUBLIC_URL+"/MDDbbasket.png"}/><p className="navbar_icon_text">Logout</p></Link>
+                        </div>
+                        : ""
+                }
+            </div>
+        </section>
     )
 }
